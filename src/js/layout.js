2e6d67f46765a57887930ca8323134dff59e2e49
andressies <huesose@gmail.com>
2019-05-13 21:27:10 +0000

    firstcommit

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,31 +2,33 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
+import { Home } from "./views/home.js";
+import { Demo } from "./views/demo.js";
+import { Single } from "./views/single.js";
+import injectContext from "./store/appContext.js";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Navbar } from "./component/navbar.js";
+import { Footer } from "./component/footer.js";
+import { BartenderList } from "./views/bartender_list.js";
 
 //create your first component
 export class Layout extends React.Component {
 	render() {
-
-        //the basename is used when your project is published in a subdirectory and not in the root of the domain
-        // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-        const basename = process.env.BASENAME || '';
+		//the basename is used when your project is published in a subdirectory and not in the root of the domain
+		// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+		const basename = process.env.BASENAME || "";
 
 		return (
 			<div className="d-flex flex-column h-100">
 				<BrowserRouter basename={basename}>
 					<ScrollToTop>
 						<Navbar />
+
 						<Switch>
 							<Route exact path="/" component={Home} />
 							<Route path="/demo" component={Demo} />
 							<Route path="/single/:theid" component={Single} />
+							<Route path="/bartender/list" component={BartenderList} />
 							<Route render={() => <h1>Not found!</h1>} />
 						</Switch>
 						<Footer />
---
+++ src/js/views/bartender_list.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class BartenderList extends React.Component {
+	render() {
+		return <h1>asdadas</h1>;
+	}
+}
